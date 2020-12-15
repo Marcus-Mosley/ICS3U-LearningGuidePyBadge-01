a0c4305f27d4c1f2a7c11fb110b1691fdb18d471
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-12-15 19:29:33 +0000

    Added in code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Created by Marcus A. Mosley
+# Created on December 2020
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass  # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
